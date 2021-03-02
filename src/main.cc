44d6c1ef12d6ebb62405d3d72dc44f1f619e3b55
lidorY <lidor323@gmail.com>
2021-03-02 21:16:37 +0200

    Defining basic view frustum class

--- src/main.cc
+++ src/main.cc
@@ -29,6 +29,35 @@ class Sphere {
 //	- Decide which are visible by once again traversing object for collision detection
 // 
 
+
+class ViewFrustum {
+public:
+	ViewFrustum(std::size_t scr_width, 
+		std::size_t scr_height, float angle) :
+		width_(scr_width),
+		height_(scr_height),
+		aspect_ratio_(static_cast<float>(scr_width)/static_cast<float>(scr_height)),
+		angle_(angle)
+	{}
+
+	Ray GetRayByPixel(int x, int y) {
+		Ray res;
+		
+		// res.direction = ...
+		// For simplicity I'm setting the "Camera" to the world origin (0,0,0)
+		res.origin = { 0,0,0 };
+
+		return res;
+	}
+
+private:
+	std::size_t width_;
+	std::size_t height_;
+	float aspect_ratio_;
+	float angle_;
+};
+
+
 void Tracer(Screen& scr) {
 	for (auto i = 0; i < scr.Height(); ++i) {
 		for (auto j = 0; j < scr.Width(); ++j) {
