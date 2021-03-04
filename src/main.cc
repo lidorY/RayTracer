ac7c345d41bd9e7a729a7003e18d1c6bd895d280
LidorY <lidor323@gmail.com>
2021-03-04 21:40:58 +0200

    a

--- src/main.cc
+++ src/main.cc
@@ -25,6 +25,23 @@ public:
 	virtual Color Shade(gmtl::Vec3d point_light, gmtl::Vec3d intersec_point) = 0;
 
 };
+
+class Cube : public Object{
+
+public:
+	std::optional<gmtl::Vec3d> TestCollision(const Ray& r) const override {
+		
+	}
+
+	Color Shade(gmtl::Vec3d point_light, gmtl::Vec3d intersec_point) override {
+		
+	}
+
+private:
+	gmtl::Vec3d bounds[8];
+	gmtl::Vec3d max, min;
+
+};
 // Basic starting object
 class Sphere : public Object{
 public:
@@ -67,7 +84,7 @@ public:
 		gmtl::normalize(light_dir);
 
 
-		double kd = 1.2;
+		double kd = 1.1;
 		double ka = 0.3;
 
 		double fctr {0.0};
@@ -88,60 +105,6 @@ private:
 	int num;
 };
 
-// Algorithm:
-// find intersections to objects in the scene
-// Draw "shadow feelers" to the existing light sources
-//	- Decide which are visible by once again traversing object for collision detection
-// 
-
-
-//class ViewFrustum {
-//public:
-//	ViewFrustum(std::size_t scr_width, 
-//		std::size_t scr_height, double angle,
-//		double view_plane_distance) :
-//		scr_width_(scr_width),
-//		scr_height_(scr_height),
-//		angle_(angle),
-//		view_plane_distance_(view_plane_distance)
-//	{
-//		aspect_ratio_ = static_cast<double>(scr_height) / static_cast<double>(scr_width);
-//		double phy_width = 2 * view_plane_distance * std::tan(angle / 2);
-//		double phy_height = aspect_ratio_ * phy_width;
-//
-//		pixel_width_ = phy_width / scr_width;
-//		pixel_height_ = phy_height / scr_height;
-//	}
-//
-//	Ray GetRayByPixel(int x, int y) {
-//		// Zero terminated indexing.(staring from x/y=0)
-//		Ray res;
-//	
-//		// For simplicity I'm setting the "Camera" to the world origin (0,0,0)
-//		res.p1 = {
-//			(x * pixel_width_) + (pixel_width_ / 2.f),
-//			(y * pixel_height_) + (pixel_height_ / 2.f),
-//			0.3 };
-//		res.p0 = { 
-//			(x * pixel_width_)  + (pixel_width_/2.f), 
-//			(y * pixel_height_) + (pixel_height_/2.f),
-//			view_plane_distance_ };
-//		return res;
-//	}
-//
-//private:
-//
-//	std::size_t scr_width_;
-//	std::size_t scr_height_;
-//	double aspect_ratio_;
-//	double angle_;
-//
-//	double view_plane_distance_;
-//	double pixel_width_;
-//	double pixel_height_;
-//};
-
-
 void Tracer(Screen& scr) {
 	//ViewFrustum vfr{scr.Width(), scr.Height(), 60.0, 1000.0};
 
@@ -158,9 +121,7 @@ void Tracer(Screen& scr) {
 			// Test for collision
 			if (auto ip = sphr.TestCollision(ray)) {
 				// Collision detected
-				scr.StorePixel(x, y, sphr.Shade(gmtl::Vec3d{ 10, 0, 0}, ip.value()));
-				// Send shadow/light feelers
-				// calculate res color based on light source
+				scr.StorePixel(x, y, sphr.Shade(gmtl::Vec3d{ 10, 0, -50}, ip.value()));
 			} 
 		}
 	}
